a2db9a136de3bcfcf7892f45eb61c041b2a55fa3
carlosgalvezp <carlosgalvezp@gmail.com>
2016-06-24 11:48:04 +0200

    Remove pointers from IMU, Baro and RC

--- src/ArduinoFC/MPU6050.cpp
+++ src/ArduinoFC/MPU6050.cpp
@@ -25,28 +25,28 @@ namespace IMU
 		return 0;
 	}
 
-	uint8_t getData(IMUData* const data)
+	uint8_t getData(IMUData& data)
 	{
 		// Burst read all the registers, starting from ACC_X_MSB
 		if (I2C::readReg(IMU_DEV_ADDR, IMU_REG_ACC_X_MSB, rawData, N_BYTES_BURST_READ)) return 1;
 
 		// Store the raw data
-		data->acc_raw.x = ((int16_t)rawData[0]) << 8 | rawData[1];
-		data->acc_raw.y = ((int16_t)rawData[2]) << 8 | rawData[3];
-		data->acc_raw.z = ((int16_t)rawData[4]) << 8 | rawData[5];
+		data.acc_raw.x = ((int16_t)rawData[0]) << 8 | rawData[1];
+		data.acc_raw.y = ((int16_t)rawData[2]) << 8 | rawData[3];
+		data.acc_raw.z = ((int16_t)rawData[4]) << 8 | rawData[5];
 
-		data->gyro_raw.x = ((int16_t)rawData[8]) << 8 | rawData[9];
-		data->gyro_raw.y = ((int16_t)rawData[10]) << 8 | rawData[11];
-		data->gyro_raw.z = ((int16_t)rawData[12]) << 8 | rawData[13];
+		data.gyro_raw.x = ((int16_t)rawData[8]) << 8 | rawData[9];
+		data.gyro_raw.y = ((int16_t)rawData[10]) << 8 | rawData[11];
+		data.gyro_raw.z = ((int16_t)rawData[12]) << 8 | rawData[13];
 
 		// Convert to real values
-		//data->acc.x = ACC_SENSITIVITY * data->acc_raw.x; // Not needed, the filter already normalizes
-		//data->acc.y = ACC_SENSITIVITY * data->acc_raw.y;
-		//data->acc.z = ACC_SENSITIVITY * data->acc_raw.z;
+		//data.acc.x = ACC_SENSITIVITY * data.acc_raw.x; // Not needed, the filter already normalizes
+		//data.acc.y = ACC_SENSITIVITY * data.acc_raw.y;
+		//data.acc.z = ACC_SENSITIVITY * data.acc_raw.z;
 
-		data->gyro.x = GYRO_SENSITIVITY * data->gyro_raw.x;
-		data->gyro.y = GYRO_SENSITIVITY * data->gyro_raw.y;
-		data->gyro.z = GYRO_SENSITIVITY * data->gyro_raw.z;
+		data.gyro.x = GYRO_SENSITIVITY * data.gyro_raw.x;
+		data.gyro.y = GYRO_SENSITIVITY * data.gyro_raw.y;
+		data.gyro.z = GYRO_SENSITIVITY * data.gyro_raw.z;
 
 		return 0;
 	}
