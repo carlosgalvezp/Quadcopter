a9250fda9b792792131605c9c94f4e956defd383
carlosgalvezp <carlosgalvezp@gmail.com>
2016-08-04 20:21:35 +0200

    Move GoogleTest to top level to avoid duplication
    The main issue was to add_subdirectory from a lower-level directory:
    
    add_subdirectory(../common/3rdparty...)
    
    The solution is to specifiy where this should be built, as a second
    argument to add_subdirectory.

--- FlightController/test/test1.cpp
+++ FlightController/test/test1.cpp
@@ -2,11 +2,5 @@
 
 TEST(Class, Function)
 {
-    EXPECT_EQ(1, 1);
-}
-
-int main(int argc, char** argv)
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    EXPECT_EQ(1, 0);
 }
