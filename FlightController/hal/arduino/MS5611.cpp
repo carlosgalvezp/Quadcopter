4bdc5abb73b3d586d4dd9af338215a9fb4c8c7e6
Carlos Galvez <carlos.galvez@volvocars.com>
2016-09-19 13:01:00 +0200

    Remove defines in Barometer

--- FlightController/hal/arduino/MS5611.cpp
+++ FlightController/hal/arduino/MS5611.cpp
@@ -1,10 +1,13 @@
 #include "MS5611.h"
 
+namespace Barometer
+{
+
 // Coefficients read from PROM to be used in calculations
-uint16_t prom_coefficients[BARO_PROM_N_COEFFS];
+uint16_t prom_coefficients[kBaroPromNCoeffs];
 
 // Buffer to read I2C data
-uint8_t data[BARO_READ_N_BYTES];
+uint8_t data[kBaroNBytesToRead];
 
 // Memory of values in case we don't have time to read
 float old_pressure, old_temperature;
@@ -14,13 +17,13 @@ uint32_t d1, d2;
 uint8_t state;
 unsigned long last_t;
 
-uint8_t Barometer::init()
+uint8_t init()
 {
 	// Read values from PROM and store for future use
 	uint8_t data[2];
-	for (uint8_t i = 0; i < BARO_PROM_N_COEFFS; ++i)
+    for (uint8_t i = 0; i < kBaroPromNCoeffs; ++i)
 	{
-		if (I2C::readReg(BARO_DEV_ADDR, BARO_REG_PROM | ((i+1) << 1), data, 2)) return 1;
+        if (I2C::readReg(kBaroI2CAddr, kBaroRegProm | ((i+1) << 1), data, 2)) return 1;
 		prom_coefficients[i] = (uint16_t)data[0] << 8 | (uint16_t)data[1];
 	}
 
@@ -28,7 +31,7 @@ uint8_t Barometer::init()
 	return 0;
 }
 
-uint8_t Barometer::getData(float& pressure, float& temperature)
+uint8_t getData(float& pressure, float& temperature)
 {
 	pressure = old_pressure;
 	temperature = old_temperature;
@@ -36,36 +39,36 @@ uint8_t Barometer::getData(float& pressure, float& temperature)
 	// State machine in order to avoid active wait when reading the sensor registers
 	switch (state)
 	{
-		case BARO_STATE_IDLE: // Start D1 conversion
-			if (I2C::writeReg(BARO_DEV_ADDR, BARO_REG_D1_OSR_256, NULL, 0)) return 1; 
+        case kBaroStateIdle: // Start D1 conversion
+            if (I2C::writeReg(kBaroI2CAddr, kBaroRegD1OSR256, NULL, 0)) return 1;
 			last_t = micros();
-			state = BARO_STATE_STARTED_D1_CONVERSION;
+            state = kBaroStateStartedD1Conv;
 			return 1;
 
-		case BARO_STATE_STARTED_D1_CONVERSION: // Read D1 and start D2 conversion
-			if ( (micros() - last_t) > BARO_CONVERSION_DELAY_US)
+        case kBaroStateStartedD1Conv: // Read D1 and start D2 conversion
+            if ( (micros() - last_t) > kBaroConversionDelayUs)
 			{
-				if (I2C::readReg(BARO_DEV_ADDR, BARO_REG_ADC_READ, data, BARO_READ_N_BYTES)) return 1; // Read D1 data
+                if (I2C::readReg(kBaroI2CAddr, kBaroRegAdcRead, data, kBaroNBytesToRead)) return 1; // Read D1 data
 				d1 = (uint32_t)data[0] << 16 | (uint32_t)data[1] << 8 | (uint32_t)data[2];
 				
-				if (I2C::writeReg(BARO_DEV_ADDR, BARO_REG_D2_OSR_256, NULL, 0)) return 1; // Initiate D2 conversion
+                if (I2C::writeReg(kBaroI2CAddr, kBaroRegD2OSR256, NULL, 0)) return 1; // Initiate D2 conversion
 				last_t = micros();
-				state = BARO_STATE_STARTED_D2_CONVERSION;
+                state = kBaroStateStartedD2Conv;
 			}
 			return 1;
 			
 
-		case BARO_STATE_STARTED_D2_CONVERSION:
-			if ((micros() - last_t) > BARO_CONVERSION_DELAY_US)
+        case kBaroStateStartedD2Conv:
+            if ((micros() - last_t) > kBaroConversionDelayUs)
 			{
-				if (I2C::readReg(BARO_DEV_ADDR, BARO_REG_ADC_READ, data, BARO_READ_N_BYTES)) return 1; // Read data
+                if (I2C::readReg(kBaroI2CAddr, kBaroRegAdcRead, data, kBaroNBytesToRead)) return 1; // Read data
 				d2 = (uint32_t)data[0] << 16 | (uint32_t)data[1] << 8 | (uint32_t)data[2];
 
 				// After reading all the data, we can compute
 				computeValues(pressure, temperature);
 				old_pressure = pressure;
 				old_temperature = temperature;
-				state = BARO_STATE_IDLE;
+                state = kBaroStateIdle;
 				return 0; 
 			}
 			return 1;
@@ -74,7 +77,7 @@ uint8_t Barometer::getData(float& pressure, float& temperature)
 	}
 }
 
-void Barometer::computeValues(float& pressure, float& temperature)
+void computeValues(float& pressure, float& temperature)
 {
 	// Compute pressure and temperature according to datasheet
 	int32_t dT = (int32_t)d2 - ((int32_t)prom_coefficients[4] << 8);
@@ -86,3 +89,5 @@ void Barometer::computeValues(float& pressure, float& temperature)
 
 	pressure = 0.01f * (((((int64_t)d1 * sens) >> 21) - off) >> 15);
 }
+
+}  // namespace Barometer
--- FlightController/hal/arduino/include/MS5611.h
+++ FlightController/hal/arduino/include/MS5611.h
@@ -6,31 +6,32 @@
 #include "I2C.h"
 #include "Arduino.h"
 
-#define BARO_DEV_ADDR		0x77
-
-// Calculation constants
-#define BARO_P_MIN			10		// mbar
-#define BARO_P_MAX			1200	// mbar
-#define BARO_T_MIN			-40		// ºC
-#define BARO_T_MAX			85		// ºC
-#define BARO_T_REF			20		// ºC
-
-#define BARO_PROM_N_COEFFS	6		// Number of coefficients to be read from PROM
-#define BARO_READ_N_BYTES	3		// Number of bytes to be read from either pressure or temperature
-
-#define BARO_REG_PROM				0xA0
-#define BARO_REG_D1_OSR_256			0x40
-#define BARO_REG_D2_OSR_256			0x50
-#define BARO_REG_ADC_READ			0x00
-
-#define BARO_CONVERSION_DELAY_US	1000 // To make sure we read correct values
-
-#define BARO_STATE_IDLE							0
-#define BARO_STATE_STARTED_D1_CONVERSION		1
-#define BARO_STATE_STARTED_D2_CONVERSION		2
-
 namespace Barometer
 {
+    // I2C device address
+    const uint8_t kBaroI2CAddr = 0x77U;
+
+    // Calculation constants
+    const uint8_t  kBaroPMin  = 10U;		// mbar
+    const uint16_t kBaroPMax  = 1200U;	    // mbar
+    const int8_t   kBaroTMin  = -40;		// ºC
+    const uint8_t  kBaroTMaxX = 85U;		// ºC
+    const uint8_t  kBaroTRef  = 20U;		// ºC
+
+    const uint8_t kBaroPromNCoeffs = 6U;  // Number of coefficients to be read from PROM
+    const uint8_t kBaroNBytesToRead	= 3U; // Number of bytes to be read from either pressure or temperature
+
+    const uint8_t kBaroRegProm     = 0xA0U;
+    const uint8_t kBaroRegD1OSR256 = 0x40U;
+    const uint8_t kBaroRegD2OSR256 = 0x50U;
+    const uint8_t kBaroRegAdcRead  = 0x00U;
+
+    const uint16_t kBaroConversionDelayUs = 1000U; // To make sure we read correct values
+
+    const uint8_t kBaroStateIdle          = 0U;
+    const uint8_t kBaroStateStartedD1Conv = 1U;
+    const uint8_t kBaroStateStartedD2Conv = 2U;
+
 	uint8_t init();
 	uint8_t getData(float& pressure, float& temperature);
 
