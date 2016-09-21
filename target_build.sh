de2fa3c093e795a9f8c06d92efde0d66f760ef3b
Carlos Galvez <carlos.galvez@volvocars.com>
2016-09-21 13:13:13 +0200

    Use CMake 3.3 in Travis

--- target_build.sh
+++ target_build.sh
@@ -2,5 +2,5 @@
 cd FlightController
 mkdir build
 cd build
-cmake .. -DARDUINO_TOOLCHAIN_FILE=../cmake/ArduinoToolchain.cmake -DARDUINO_1_5=ON
+/opt/cmake-3.3.2-Linux-x86_64/bin/cmake .. -DARDUINO_TOOLCHAIN_FILE=../cmake/ArduinoToolchain.cmake -DARDUINO_1_5=ON
 make
