44a8942e95a901f16afd82d4109ee7a223f3a159
Carlos Galvez <carlosgalvezp@gmail.com>
2017-06-30 22:39:09 +0200

    Add job to build the Flight Controller

--- run.py
+++ run.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
+import argparse
+import sys
+import os
+import shutil
+
 from tools.pyjobs.build_docker import DockerBuilder
+from tools.pyjobs.build_fc import FlightControllerBuilder
 
+def main(cmd):
+    if cmd == 'build-docker':
+        DockerBuilder().run()
 
-def main():
-    DockerBuilder().run()
+    elif cmd == 'build-fc':
+        FlightControllerBuilder().run()
+
+    elif cmd == 'clean':
+        if os.path.exists('build'):
+            shutil.rmtree('build')
+    else:
+        print('Unknown command {}'.format(cmd))
+        return -1
+
+    return 0
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
---
+++ tools/pyjobs/build_fc.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+
+class FlightControllerBuilder(object):
+    def __init__(self):
+        self._this_path = os.path.dirname(os.path.realpath(__file__))
+        self._root = os.path.abspath(os.path.join(self._this_path, '..', '..'))
+        self._build_dir = os.path.join(self._root, 'build', 'fc')
+        self._fc_root = os.path.join(self._root, 'FlightController')
+        self._toolchain = os.path.join(self._fc_root, 'cmake',
+                                       'ArduinoToolchain.cmake')
+        self._image = 'carlosgalvezp/quadcopter'
+
+        self._uid = os.getuid()
+        self._gid = os.getgid()
+
+    def run(self):
+        # Create directory if it doesn't exist
+        if not os.path.exists(self._build_dir):
+            os.makedirs(self._build_dir)
+
+        self._run_cmake()
+        self._run_make()
+
+    def _run_cmake(self):
+        cmd = ['docker', 'run',
+               '--rm=true',
+               '--volume={}:{}'.format(self._root, self._root),
+               '--user={}:{}'.format(self._uid, self._gid),
+               '--workdir={}'.format(self._build_dir),
+               self._image, 'cmake', self._fc_root,
+               '-DCMAKE_TOOLCHAIN_FILE={}'.format(self._toolchain),
+               '-DARDUINO_1_5=ON']
+        subprocess.run(cmd, check=True)
+
+    def _run_make(self):
+        cmd = ['docker', 'run',
+               '--rm=true',
+               '--volume={}:{}'.format(self._root, self._root),
+               '--user={}:{}'.format(self._uid, self._gid),
+               '--workdir={}'.format(self._build_dir),
+              self._image, 'make']
+        subprocess.run(cmd, check=True)
