c25f928d31a0cfd409b8459e7bb486e187ab0c3e
Carlos Galvez <carlos.galvez@volvocars.com>
2017-07-05 09:21:21 +0200

    Mount the device optionally if it exists when running the GUI

--- tools/pyjobs/gui.py
+++ tools/pyjobs/gui.py
@@ -54,17 +54,26 @@ class GUIRunner(object):
         self._uid = os.getuid()
         self._gid = os.getgid()
 
+        self._device = '/dev/ttyUSB0'
+
     def run(self):
         cmd = ['docker', 'run',
                '--rm=true',
-               '--tty=true',
-               '--device=/dev/ttyUSB0',
-               '--volume={}:{}'.format(self._root, self._root),
-               '--user={}:{}'.format(self._uid, self._gid),
-               '--group-add=dialout',
-               '--env=DISPLAY',
-               '--volume=/tmp/.X11-unix:/tmp/.X11-unix',
-               '--workdir={}'.format(self._build_dir),
-              self._image,
-              './CriusGUI']
+               '--tty=true']
+
+        if os.path.exists(self._device):
+            cmd.extend('--device={}'.format(self._device))
+        else:
+            print('WARNING: {} not found. ' \
+                  'Please connect the Crius board'.format(self._device))
+
+        cmd.extend(['--volume={}:{}'.format(self._root, self._root),
+                    '--user={}:{}'.format(self._uid, self._gid),
+                    '--group-add=dialout',
+                    '--env=DISPLAY',
+                    '--volume=/tmp/.X11-unix:/tmp/.X11-unix',
+                    '--workdir={}'.format(self._build_dir),
+                    self._image,
+                    './CriusGUI'])
+
         subprocess.check_call(cmd)
